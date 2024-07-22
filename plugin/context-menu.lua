1a5b44b057181dbc2afbf3beb08b56d7da7b18ce
Lintao <lintao.amons@outlook.com>
2024-07-22 18:03:12 +0800

    merge config and setup callable in runtime

---
+++ plugin/context-menu.lua
@@ -0,0 +1,6 @@
+local default_config = {
+  menu_items = {}, -- override the default items:: use it when you don't want the plugin provided menu_items
+  enable_log = true, -- enable error log be printed out. Turn it off if you don't want see those lines
+}
+
+vim.g.context_menu_config = default_config
